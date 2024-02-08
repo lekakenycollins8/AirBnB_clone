a899e39058d90882b9f54aa2f4a22d6b823f8b21
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-02-08 06:17:26 -0800

    handled do destroy

--- console.py
+++ console.py
@@ -57,8 +57,31 @@ class HBNBCommand(cmd.Cmd):
             print(storage.all()[key])
         else:
             print("** no instance found **")
-        
+            return
 
+    def do_destroy(self, arg):
+        """ Deletes an instance based on the class name and
+        id (save the change into the JSON file)
+        """
+        args = arg.strip().split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in storage.allclasses():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
+        if key in storage.all().keys():
+            del storage.all()[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+            return
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
