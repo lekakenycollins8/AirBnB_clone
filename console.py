9ec8ca7f1d2723c3ea40848ee97b9c0e56e28521
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-02-12 01:14:50 -0800

    added default method

--- console.py
+++ console.py
@@ -80,9 +80,9 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** no instance found **")
             return
-
+    
     def do_all(self, arg):
-        """prints string representation of instances based on not on class name"""
+        """prints string representation of instances basedon class name"""
         if not arg:
             instances = storage.all().values()
         else:
@@ -95,7 +95,7 @@ class HBNBCommand(cmd.Cmd):
                     (obj).__name__ == class_name]
         str_instances = [str(obj) for obj in instances]
         print(str_instances)
-
+    
     def do_update(self, arg):
         """Updates an instance based on the class name and id
         by adding or updating attribute (save the change into the JSON file).
@@ -141,35 +141,24 @@ class HBNBCommand(cmd.Cmd):
                 counter += 1
         print(counter)
 
-    def parse_command(self, command):
-        """Parse the command to extract class name, command, and ID."""
-        pattern = r'^(\w+)\.(\w+)\(([^)]+)\)$'
-        match = re.match(pattern, command)
-        if match:
-            class_name = match.group(1)
-            command_name = match.group(2)
-            args = match.group(3).split(',')
-            return class_name, command_name, args
-        else:
-            return None, None, None
 
-    def default(self, line):
+    def default(self, arg):
         """Called on an input line when the command prefix is not recognized."""
-        class_name, command_name, args = self.parse_command(line)
-        if class_name and command_name and args:
-            if command_name == 'show':
-                # Call the show method
-                self.do_show(f"{class_name} {' '.join(args)}")
-            elif command_name == 'destroy':
-                # Call the destroy method
-                self.do_destroy(f"{class_name} {' '.join(args)}")
-            elif command_name == 'update':
-                # Call the update method
-                self.do_update(f"{class_name} {' '.join(args)}")
+        args = arg.split(".")
+        if len(args) == 2 and args[1].endswith("()"):
+            class_name, method_name = args[0], args[1][:-2]
+            if class_name in storage.allclasses():
+                cls = storage.allclasses()[class_name]
+                method = getattr(cls, method_name, None)
+                if method and callable(method):
+                    method()
+                else:
+                    print("** method doesn't exist **")
             else:
-                print("** Invalid command name **")
+                print("** class doesn't exist **")
         else:
-            print("** Invalid command syntax **")
+            print("** invalid command **")
+
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ Module with the Parent class: BaseModel for the AirBnB clone project"""
 
 
